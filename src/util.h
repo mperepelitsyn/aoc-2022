53e563e4f33421038286157e21f21f57154b8dd3
Maxim Perepelitsyn <mperepelitsyn@gmail.com>
2022-12-22 20:22:52 -0800

    Reformat util.h

--- src/util.h
+++ src/util.h
@@ -1,39 +1,42 @@
 #pragma once
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <filesystem>
 
 inline std::ifstream getInputStream(const char *fname) {
 #ifdef INPUT_PATH
-    std::filesystem::current_path(INPUT_PATH);
+  std::filesystem::current_path(INPUT_PATH);
 #endif
-    return std::ifstream{fname};
+  return std::ifstream{fname};
 }
 
-template<class T, size_t N>
-std::ostream& operator <<(std::ostream &os, const std::array<T, N> &arr) {
-    for (auto &e : arr)
-        os << e << std::endl;
-    return os;
+template <class T, size_t N>
+std::ostream &operator<<(std::ostream &os, const std::array<T, N> &arr) {
+  for (auto &e : arr) os << e << std::endl;
+  return os;
 }
 
-template<class T, class U>
+template <class T, class U>
 inline void outputAndVerify(const char *desc, T actual, U expected) {
-    if (actual != expected) {
-        std::cerr << "FAIL: " << desc << ": " << "expected " << expected << ", actual " << actual << std::endl;
-        std::exit(1);
-    } else {
-        std::cout << desc << ": " << actual << std::endl;
-    }
+  if (actual != expected) {
+    std::cerr << "FAIL: " << desc << ": "
+              << "expected " << expected << ", actual " << actual << std::endl;
+    std::exit(1);
+  } else {
+    std::cout << desc << ": " << actual << std::endl;
+  }
 }
 
-template<class T, size_t N>
+template <class T, size_t N>
 inline void outputAndVerify(const char *desc, const std::array<T, N> &actual, const std::array<T, N> &expected) {
-    if (actual != expected) {
-        std::cerr << "FAIL: " << desc << ": " << "expected:" << std::endl << expected << "actual:" << std::endl << actual;
-        std::exit(1);
-    } else {
-        std::cout << desc << ":" << std::endl << actual;
-    }
+  if (actual != expected) {
+    std::cerr << "FAIL: " << desc << ": "
+              << "expected:" << std::endl
+              << expected << "actual:" << std::endl
+              << actual;
+    std::exit(1);
+  } else {
+    std::cout << desc << ":" << std::endl << actual;
+  }
 }
